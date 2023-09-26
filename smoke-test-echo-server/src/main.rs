f7084c1a296c5b1be487fc676b4134483cffa0cf
Amidamaru <thaodt@users.noreply.github.com>
2023-09-26 15:10:32 +0700

    fix: 1st challenge

--- smoke-test-echo-server/src/main.rs
+++ smoke-test-echo-server/src/main.rs
@@ -1,35 +1,33 @@
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::TcpListener;
+use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let listener = TcpListener::bind("0.0.0.0:6969").await?;
-    println!("Ready to accept connections");
+    println!("Ready to accept connection");
     loop {
-        let (mut socket, _) = listener.accept().await?;
+        let (stream, _) = listener.accept().await?;
 
+        // handle(stream).await.expect("failed to process connection");
         tokio::spawn(async move {
-            let mut buf: Vec<u8> = Vec::new();
-            loop {
-                let _ = match socket.read_to_end(&mut buf).await {
-                    // socket closed
-                    Ok(n) if n == 0 => return,
-                    Ok(n) => n,
-                    Err(e) => {
-                        eprintln!("Failed to read from socket; err = {:?}", e);
-                        return;
-                    }
-                };
-
-                let message = String::from_utf8_lossy(&buf);
-                println!("Message: {:?}", message);
-
-                // Write the data back
-                if let Err(e) = socket.write_all(&buf).await {
-                    eprintln!("failed to write to socket; err = {:?}", e);
-                    return;
-                }
+            if let Err(e) = handle(stream).await {
+                println!("failed to process connection; error = {:?}", e);
             }
         });
     }
+}
+
+async fn handle(mut stream: TcpStream) -> Result<(), Box<dyn std::error::Error>> {
+    loop {
+        let mut buf = [0u8; 4096];
+        let n = stream.read(&mut buf).await?;
+        if n == 0 {
+            break;
+        }
+        stream.write_all(&buf[..n]).await?;
+    }
+
+    stream.shutdown().await?;
+
+    Ok(())
 }
